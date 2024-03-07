f5a84f3173724c11590a8c09a7d827f3b410e2fb
MaxaProgy <mariyazz@inbox.ru>
2024-03-07 20:21:04 +0300

    7.sh fixed

--- lab2/7.sh
+++ lab2/7.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Сбор информации о процессах и их использовании памяти
+output_file="3_process_with_max_mem_in_minute.out"
+
 collect_proc_info() {
     ps -Ao pid,command | tail -n +2 | awk '{print $1":"$2}' |
     while read -r tmp; do
         pid=$(echo $tmp | awk -F ":" '{print $1}')
         com=$(echo $tmp | awk -F ":" '{print $2}')
-        pth="/proc/$pid"
-        if [ -f "$pth/io" ]; then
-            byte=$(grep -h "read_bytes:" "$pth/io" | sed "s/[^0-9]*//")
+        
+        path="/proc/$pid"
+        if [ -f "$path/io" ]; then
+            byte=$(grep -h "read_bytes:" "$path/io" | sed "s/[^0-9]*//")
             echo "$pid $com $byte"
         fi
     done | sort -nk1
 }
 
-# Сбор информации о процессах через минуту
+collect_proc_info > temp_info_1
 sleep 1m
-collect_proc_info > _b
+collect_proc_info > temp_info_2
 
-# Сравнение и вывод результатов
-paste _a _b | awk '{if ($1 == $4) print $1, $2, $3, $5}' | 
+paste temp_info_1 temp_info_2 | awk '{if ($1 == $4) print $1, $2, $3, $6}' | 
 while read -r pid com mem1 mem2; do
     res=$(echo "$mem2-$mem1" | bc)
-    echo "$pid : $com : $res"
-done | sort -nrk3 | head -3 > 7.out
+    echo "$pid : $com : $(echo "$mem2-$mem1" | bc)"
+done |  sort -t ':' -nr -k3 | head -3 > "$output_file"
